356bd66ca01c66597631e87cd1f6ea8df23fcee2
tanjan-sj <tanjan.sj@gmail.com>
2022-08-08 01:46:23 +0600

    wip: send conn request, issue creds

--- api-routes/routes.js
+++ api-routes/routes.js
@@ -1,13 +1,101 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/hello', async function (req, res, next) {
+const axios = require('axios');
+const http = require('http');
+const QRCode = require('qrcode')
+
+router.post('/hello', async function (req, res, next) {
+    const responseBody = {};
+
     try{
-        res.status(200).json({success: "hello world"});
+        axios.post('http://localhost:8021/connections/create-invitation')
+            .then(response => {
+                const invitationUrl = response.data.invitation_url;
+                QRCode.toDataURL(invitationUrl, function (err, url) {
+                    responseBody.invitation_url = invitationUrl;
+                    responseBody.invitation_qr = url;
+                    res.status(200).json(responseBody);
+                })
+            })
+            .catch(error => {
+                console.log(error);
+                res.status(error.statusCode || 500).json({ message: error.message });
+            });
     }catch(error){
         console.log(error);
         res.status(error.statusCode || 500).json({ message: error.message });
     }
 });
+
+router.post('/send-offer', async function (req, res, next) {
+    const responseBody = {};
+    console.log("-----------------------------------------------")
+    console.log("req: ", req.body);
+
+    let config = {
+        headers: {
+            'accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }
+
+    try{
+        axios.post('http://localhost:8021/issue-credential/send-offer', JSON.stringify(req.body), config)
+            .then(response => {
+                //console.log("response: ", response);
+                console.log("areh");
+                res.status(200).json({status: true});
+                // const invitationUrl = response.data.invitation_url;
+                // QRCode.toDataURL(invitationUrl, function (err, url) {
+                //     responseBody.invitation_url = invitationUrl;
+                //     responseBody.invitation_qr = url;
+                //     res.status(200).json(responseBody);
+                // })
+            })
+            .catch(error => {
+                console.log(error);
+                res.status(error.statusCode || 500).json({ message: error.message });
+            });
+    }catch(error){
+        console.log(error);
+        res.status(error.statusCode || 500).json({ message: error.message });
+    }
+});
+
+// router.post('/send-message', async function (req, res, next) {
+//     const responseBody = {};
+//     console.log("req: ", req.body);
+//     try{
+//         //const conn_id = req.body['connection_id'];
+//         //console.log("conn_id: ", conn_id);
+//         axios.post('http://localhost:8021/connections/b6e6cf16-66ac-441f-b2f3-766887b3b90b/send-message')
+//             .then(response => {
+//                 console.log("hey");
+//                 console.log(response.data);
+//                 // const invitationUrl = response.data.invitation_url;
+//                 // QRCode.toDataURL(invitationUrl, function (err, url) {
+//                 //     //console.log(url);
+//                 //     responseBody.invitation_url = invitationUrl;
+//                 //     responseBody.invitation_qr = url;
+//                 //     res.status(200).json(responseBody);
+//                 // })
+//             })
+//             .catch(error => {
+//                 //console.log(error);
+//                 res.status(error.statusCode || 500).json({ message: error.message });
+//             });
+//         res.status(200).json({success: true});
+//
+//     }catch(error){
+//         console.log(error)
+//
+//         res.status(error.statusCode || 500).json({ message: error.message });
+//     }
+// });
+
+// /Users/sumayajannat/Library/Application Support/ngrok/ngrok.yml
+//ngrok start --all
+
 
 module.exports = router;
--- app.js
+++ app.js
@@ -7,5 +7,39 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', ariesRouter);
+app.post('/webhooks/*', async function (req, res, next) {
+    try{
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Hello World\n');
+        const conID = req.body['connection_id'];
+        console.log(conID);
+        const conStatus = req.body['rfc23_state'];
+        if(conID){
+            if(conStatus === "completed"){
+                console.log("Invitation Completed with conID:" + conID)
+            }
+            console.log("request body: ", req.body);
+            if(req.body['state'] === 'credential_acked'){
+                console.log("Credential acked...")
+            }
+            if(req.body['verified'] === 'true'){
+                console.log("Credential verified after proof request..:")
+
+                // You can now retrieve the attributes from the proof request via this way.....
+                // var base64data = JSON.stringify(req.body['presentation_request_dict']['request_presentations~attach'][0]['data']['base64'])
+                // const decodedString = Buffer.from(base64data, "base64");
+                // const jsonData = JSON.parse(decodedString.toString());
+                // const proofStatus = true
+                // const retrievedAttribute = jsonData['requested_attributes']['0_role']['value']
+            }
+        }
+        // res.status(200).json({success: "hello world"});
+
+    }catch(error){
+        console.log(error)
+
+        res.status(error.statusCode || 500).json({ message: error.message });
+    }
+});
 
 module.exports = app;
